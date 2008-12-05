92802bf4fb1d1398fdd82869d565882734c48291
Elijah Miller <elijah.miller@gmail.com>
2008-12-05 00:13:33 -0500

    Initial commit.

---
+++ examples/httpjour.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'easyjour')
+
+class HTTPjour
+  def self.serve
+    # This code is webrick example code, slightly modified
+    require 'webrick'
+    include WEBrick
+
+    port = rand(10_000) + 1025
+    s = HTTPServer.new(
+      :Port            => port,
+      :DocumentRoot    => Dir::pwd
+    )
+    Easyjour.serve("webrick-#{port}", 'http', port)
+
+    trap("INT") { s.shutdown }
+    s.start
+  end
+
+
+  def self.search(open = false)
+    puts "Searching for servers... (press control-c to stop)\n\n"
+    search = Easyjour.search('http') do |service|
+      puts " * http://#{service.target}:#{service.port}/"
+      system('open', "http://#{service.target}:#{service.port}/") if open
+    end
+    trap("INT") { exit }
+    loop { sleep 10 }
+  end
+  
+
+  def self.help
+    puts "usage: "
+    puts " serve    # serves the current directory over HTTP"
+    puts " search   # looks for HTTP servers"
+    puts " show     # opens every HTTP server as it finds them"
+    exit
+  end
+end
+
+case ARGV[0]
+when 'serve'
+  HTTPjour.serve
+when 'search'
+  HTTPjour.search
+when 'open'
+  HTTPjour.search(true)
+else
+  HTTPjour.help
+end
---
+++ lib/easyjour.rb
@@ -0,0 +1,96 @@
+require 'net/dns/mdns-sd'
+
+module Easyjour
+  # Turns a service and protocol into _service._protocol format, automatically
+  # defaults to TCP if no protocol is specified. Also gracefully handles a service
+  # that is already in the _service._protocol format format.
+  def self.type_from_parts(service, protocol = :tcp)
+    if service =~ /^_\w+\._\w+$/
+      service
+    else
+      "_#{service}._#{protocol}"
+    end
+  end
+  
+  def self.serve(name, service, port, text_record = {}, protocol = :tcp)
+    Service.new(name, service, port, text_record, protocol = :tcp)
+  end
+  
+  class Service
+    def initialize(name, service, port, text_record = {}, protocol = :tcp)
+      @service = Net::DNS::MDNSSD.register(name, Easyjour.type_from_parts(service, protocol), 'local', port, text_record)
+    end
+    
+    # Stops a service from being discoverable.
+    def stop
+      @service.stop
+    end
+  end
+  
+  # Initiaites a service search, returns an Easyjour::Search. Search will continue until stop 
+  # is called. Can be given a block for immediate result processing.
+  #
+  #  # Find the HTTP servers that respond in 5 seconds or less
+  #  search = Easyjour.search('http')
+  #  sleep 5
+  #  search.stop
+  #  search.results.each do |result|                      # access the entire result set
+  #    puts "http://#{result.target}:#{result.port}/"
+  #  end  
+  #
+  #  # Continously find HTTP servers
+  #  search = Easyjour.search('http') do |result|
+  #    puts "http://#{result.target}:#{result.port}/"
+  #  end
+  #  search.results                                       # result set is updated as servers respond
+  def self.search(service, protocol = :tcp, &block)
+    Search.new(service, protocol, &block)
+  end
+
+  # Searches for a service for timeout seconds and returns the results after stopping the search.
+  # Can be given a block for immediate result processing.
+  #
+  #  # Find the Git servers that respond in 5 seconds or less
+  #  results = Easyjour.search(5, 'git')
+  #  results.each do |result|
+  #    puts "git://#{result.target}:#{result.port}"
+  #  end
+  def self.synchronous_search(timeout, service, protocol = :tcp, &block)
+    search = Search.new(service, protocol, &block)
+    sleep timeout
+    search.stop
+    search.results
+  end
+  
+  
+  class Search
+    def initialize(service, protocol = :tcp)
+      @results = []
+      @results_mutex = Mutex.new
+      
+      @query = Net::DNS::MDNSSD.browse(Easyjour.type_from_parts(service, protocol), 'local') do |reply|
+        Net::DNS::MDNSSD.resolve(reply.name, reply.type, reply.domain) do |reply|          
+          yield(reply) if block_given?
+
+          @results_mutex.synchronize do
+            @results << reply
+          end
+        end
+      end
+    end
+    
+    # Returns the current the results.
+    def results
+      @results_mutex.synchronize do
+        @results
+      end
+    end
+    
+    # Stop the search.
+    def stop
+      @query.stop
+    end
+    
+  end
+
+end
